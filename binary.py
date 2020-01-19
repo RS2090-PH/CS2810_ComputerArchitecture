f3b7d803097ce4f629948850a103814b61ef4810
RS2090-PH <57384116+RS2090-PH@users.noreply.github.com>
2020-01-19 09:42:00 -0700

    Binary project

---
+++ binary.py
@@ -0,0 +1,53 @@
+class binary(object):
+    """
+    Class designed to allow converstion between binary and decimal values.
+    """
+    
+    def __init__(self, value):
+        """
+        ShortDescription
+        
+        FullProcessDescription
+        
+        Args:
+            Arg: ArgDescription
+        Returns:
+            Return: ReturnDescription
+        """
+        if value is (int or float):
+            self.value = value
+        else:
+            print("Value must be an integer or float data type.")
+            
+
+    def dtob(self):
+        """
+        ShortDescription
+        
+        FullProcessDescription
+        
+        Args:
+            Arg: ArgDescription
+        Returns:
+            Return: ReturnDescription
+        """
+        if "." in self.value:
+
+        else:
+
+
+    def btod(self):
+        """
+        ShortDescription
+        
+        FullProcessDescription
+        
+        Args:
+            Arg: ArgDescription
+        Returns:
+            Return: ReturnDescription
+        """
+        if "." in self.value:
+
+        else:
+            
---
+++ binary_test.py
@@ -0,0 +1,13 @@
+import binary
+
+class binary_unittest():
+    def test_bin_negative(self):
+
+    def test_dec_negative(self):
+
+    def test_decimal(self):
+
+    def test_imaginary(self):
+
+    def test_nonnumber(self):
+        
