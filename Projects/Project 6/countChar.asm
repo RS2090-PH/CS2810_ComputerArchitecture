25dd12a853a0d6ae0d4ec70dbf390bc7cab635e2
RS2090-PH <57384116+RS2090-PH@users.noreply.github.com>
2020-03-30 12:39:11 -0600

    Update

--- Projects/Project 6/countChar.asm
+++ Projects/Project 6/countChar.asm
@@ -15,15 +15,17 @@ INREG	.FILL xFE02			; label keyboard data registry
 OUTSTAT	.FILL xFE04			; label monitor status registry
 OUTREG	.FILL xFE06			; label monitor data registry
 STRING	.STRINGZ "Now is the time for all good men to come to the aid of their country."
+CTMESS	.STRINGZ " occurances of your selected character appear in the string."
 
-CIN		LDI R7, INSTAT		; Gets character from keyboard
+CIN	LDI R7, INSTAT		; Gets character from keyboard
 		BRzp CIN			; and saves to R0
 		LDI R0, INREG
-		BRnzp x03
+		BRnzp x04
 
 START	AND	R2, R2, #0		; R2 is counter, initially 0
 		LEA	R3, STRING		; R3 is pointer to string
-		BRnzp CIN					; R0 gets character input
+		LEA R4, CTMESS
+		BRnzp CIN			; R0 gets character input
 		NOT R0, R0			; Calculate two's complement
 		ADD R0, R0, #1
 		LDR	R1, R3, #0		; R1 gets first character in string 
@@ -37,13 +39,25 @@ GETCHAR	ADD	R3, R3, #1		; Point to next character.
 		LDR	R1, R3, #0		; R1 gets next char to test
 		BR	TEST
 
+SOUT	LDI R7, OUTSTAT		; Gets character from R0 and
+		BRzp SOUT			; outputs to monitor when the
+		LD R0, ASCII
+		ADD R0, R0, R4
+		STI R0, OUTREG		; monitor is ready.
+		ADD R4, R4, #1
+		BRnp SOUT
+		BR x03
+
 COUT	LDI R7, OUTSTAT		; Gets character from R0 and
 		BRzp COUT			; outputs to monitor when the
 		STI R0, OUTREG		; monitor is ready.
-		BRnzp x03
-
+		LDI R0, CTMESS
+		BRnzp SOUT
+	
 OUTPUT	LD	R0, ASCII		; Load the ASCII offset
 		ADD	R0, R0, R2		; Covert binary count to ASCII
 		BRnzp COUT				; ASCII code in R0 is displayed.
 		HALT			 	; Halt machine
 		.END
+
+	
